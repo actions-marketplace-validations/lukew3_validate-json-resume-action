05b001b0f1426964ea3dc2fbe270ddc31fbedeca
lukew3 <lukew25073@gmail.com>
2022-08-14 23:13:34 -0400

    setFailed before logging error

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ resumeSchema.validate(
   resumeObject,
   function (err, report) {
     if (err) {
-      console.error("The resume was invalid:", err);
       core.setFailed(err);
+      console.error("The resume was invalid:", err);
       return;
     }
     console.log("Resume validated successfully:", report);
   },
   function (err) {
-    console.error("The resume was invalid:", err);
     core.setFailed(err);
+    console.error("The resume was invalid:", err);
   }
 );
